6fa651f0ff93dfe405bae222f09e2714ee4cccd3
hung20012004 <131970591+hung20012004@users.noreply.github.com>
2025-10-20 18:23:36 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 
 import app from './app';
 import { env } from './config/env'; // Import từ file env an toàn
-
+import cors from 'cors';
+app.use(cors({
+  origin: [
+    'https://ffresh.io.vn',   // FE chính thức
+    'http://localhost:3000'   // dùng khi dev
+  ],
+  credentials: true
+}));
 const PORT = env.PORT || 5000;
 
 app.listen(PORT, () => {
